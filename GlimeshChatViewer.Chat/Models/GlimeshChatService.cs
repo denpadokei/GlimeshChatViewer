81de7d7ac7e3bb33da858022a9ec384e8ec21923
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-04-26 00:28:31 +0900

    ひとまずチャットへの接続まで

--- GlimeshChatViewer.Chat/Models/GlimeshChatService.cs
+++ GlimeshChatViewer.Chat/Models/GlimeshChatService.cs
@@ -1,10 +1,132 @@
-﻿using System;
+﻿using Glimesh.Base;
+using Glimesh.Base.Clients;
+using Glimesh.Base.Models.Channels;
+using Glimesh.Base.Models.Users;
+using GlimeshChatViewer.Chat.Interfaces;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GlimeshChatViewer.Chat.Models
 {
-    class GlimeshChatService
+    public class GlimeshChatService : IChatService, IDisposable
     {
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プロパティ
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // パブリックイベント
+        public event OnChatReciveEventHandler OnChatRecived;
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // コマンド用メソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // オーバーライドメソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // パブリックメソッド
+        public async Task Start()
+        {
+            this.connection = await GlimeshConnection.ConnectViaLocalhostOAuthBrowser(clientId, secretKey, scopes);
+            if (connection == null) {
+                return;
+            }
+            this.userModel = await this.connection.Users.GetCurrentUser();
+            if (this.userModel == null) {
+                return;
+            }
+            this.channelModel = await this.connection.Channel.GetChannelByName(this.userModel.username);
+            if (channelModel == null) {
+                return;
+            }
+            this.chatEventClient = await ChatEventClient.CreateWithToken(this.connection);
+            this.chatEventClient.OnChatMessageReceived += this.ChatEventClient_OnChatMessageReceived;
+            this.chatEventClient.OnFollowOccurred += this.ChatEventClient_OnFollowOccurred;
+
+            if (!await this.chatEventClient.Connect()) {
+                return;
+            }
+            if (!await this.chatEventClient.JoinChannelChat(this.channelModel.id) || !await this.chatEventClient.JoinChannelEvents(this.channelModel.id, this.userModel.id)) {
+                return;
+            }
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プライベートメソッド
+        private void ChatEventClient_OnFollowOccurred(object sender, Glimesh.Base.Models.Clients.Channel.FollowPacketModel e)
+        {
+
+        }
+        private void ChatEventClient_OnChatMessageReceived(object sender, Glimesh.Base.Models.Clients.Chat.ChatMessagePacketModel e)
+        {
+            this.OnChatRecived?.Invoke(this, new OnChatReciveEventArgs(e));
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // メンバ変数
+        private static List<OAuthClientScopeEnum> scopes = new List<OAuthClientScopeEnum>()
+        {
+            OAuthClientScopeEnum.chat,
+            OAuthClientScopeEnum.publicinfo
+        };
+
+        private GlimeshConnection connection;
+        private UserModel userModel;
+        private ChannelModel channelModel;
+        private ChatEventClient chatEventClient;
+        private string clientId;
+        private string secretKey;
+        private bool disposedValue;
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // 構築・破棄
+        public GlimeshChatService()
+        {
+#if DEBUG
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "IDs.json");
+            if (File.Exists(path)) {
+                var jsonText = File.ReadAllText(path);
+                var json = JObject.Parse(jsonText);
+                if (json.TryGetValue("client_id", out var clientId)) {
+                    this.clientId = clientId.Value<string>();
+                }
+                if (json.TryGetValue("secret_key", out var secret)) {
+                    this.secretKey = secret.Value<string>();
+                }
+            }
+#endif
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue) {
+                if (disposing) {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~GlimeshChatService()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
---
+++ GlimeshChatViewer.Chat/Models/OnChatReciveEventArgs.cs
@@ -0,0 +1,39 @@
+﻿using Glimesh.Base.Models.Clients.Chat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlimeshChatViewer
+{
+    public class OnChatReciveEventArgs : EventArgs
+    {
+        public string ID { get; private set; }
+        public string ChannelID { get; private set; }
+        public string UserID { get; private set; }
+        public string UserName { get; private set; }
+        public string DisplayName { get; private set; }
+        public string AvatarUrl { get; private set; }
+        public DateTime ConfirmedAt { get; private set; }
+        public string Message { get; private set; }
+        public DateTime SentAt { get; private set; }
+
+        public OnChatReciveEventArgs(ChatMessagePacketModel e)
+        {
+            this.ID = e.ID;
+            this.ChannelID = e.ChannelID;
+            this.UserID = e.User.id;
+            this.UserName = e.User.username;
+            this.DisplayName = e.User.displayname;
+            this.AvatarUrl = e.User.avatarUrl;
+            if (DateTime.TryParse(e.User.confirmedAt, out var confirmedAt)) {
+                this.ConfirmedAt = confirmedAt;
+            }
+            this.Message = e.Message;
+            if (DateTime.TryParse(e.SentAt, out var sentAt)) {
+                this.SentAt = sentAt;
+            }
+        }
+    }
+
+    public delegate void OnChatReciveEventHandler(object sender, OnChatReciveEventArgs e);
+}
